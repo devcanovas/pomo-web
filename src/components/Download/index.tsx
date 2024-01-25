c4c70925669f3f2cb7035109b4db55e66b2266eb
Matheus Canovas <matheuscanovas9@gmail.com>
2024-01-25 20:42:22 -0300

    adding the direct link to download

--- src/components/Download/index.tsx
+++ src/components/Download/index.tsx
@@ -4,7 +4,9 @@ export function Download() {
       <img src="/icons/logo-pomo.png" />
       <h1 className="text-2xl text-zinc-500">POMO - vBeta 0.1</h1>
       <button className="bg-[#FE4F60] p-4 rounded-full">
-        Download POMO - vBeta 0.1
+        <a href="https://raw.githubusercontent.com/devcanovas/pomov2/master/pomov2_0.0.0_x64_en-US.msi">
+          Download POMO - vBeta 0.1
+        </a>
       </button>
     </div>
   );
